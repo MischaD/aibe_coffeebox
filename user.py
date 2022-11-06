dcc8ffec295d45297851eb332d78f065e01a9c28
Michael Meiler <michael-meiler@gmx.de>
2022-11-06 21:48:55 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 
+
 @dataclass
-class User():
+class User:
     id: int
     username: str = ''
     balance: float = 0.0
     consumed: float = 0.0
     paid: float = 0.0
 
-    def calculate_debt(self,itemprice):
+    def calculate_debt(self, itemprice):
         self.balance = self.balance - itemprice
         self.consumed = self.consumed + itemprice
