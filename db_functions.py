cdd80d6a555d727cc0e6aa5bedf77cc6c177da21
Michael Meiler <michael-meiler@gmx.de>
2022-11-06 21:48:24 +0100

    Update db_functions.py

--- db_functions.py
+++ db_functions.py
@@ -21,8 +21,8 @@ def get_products_list(db_conn):
     cur.execute("SELECT * FROM products ORDER BY name ASC")  
     products_list = cur.fetchall()
     # Column 0 is name, column 1 is price
-    row1= [item[0] for item in products_list]
-    row2= [item[1] for item in products_list]
+    row1 = [item[0] for item in products_list]
+    row2 = [item[1] for item in products_list]
     items_price_dict = dict(zip(row1, row2))
 
     return items_price_dict
@@ -39,7 +39,7 @@ def get_users(db_conn):
 def get_product_price(db_conn):
     product = "lemonade"
     cur = db_conn.cursor()
-    cur.execute("SELECT price FROM products WHERE name=?",(product,))
+    cur.execute("SELECT price FROM products WHERE name=?", (product,))
 
     price = cur.fetchone()
     print("asdf", price[0])
@@ -55,10 +55,10 @@ def add_user(db_conn):
 
 def update_user_debt(db_conn, user):
     cur = db_conn.cursor()
-    cur.execute("UPDATE statistics SET balance=? WHERE id=(?)", (user.balance,user.id))
-    cur.execute("UPDATE statistics SET consumed=? WHERE id=(?)", (user.consumed,user.id))
+    cur.execute("UPDATE statistics SET balance=? WHERE id=(?)", (user.balance, user.id))
+    cur.execute("UPDATE statistics SET consumed=? WHERE id=(?)", (user.consumed, user.id))
 
-    #conn.commit()
+#    conn.commit()
 
 
 if __name__ == '__main__':
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     conn = create_connection(database)
     with conn:
         get_products_list(conn)
-        bla = (1,'kokolores')
-        update_user_debt(conn,bla)
+        bla = (1, 'kokolores')
+        update_user_debt(conn, bla)
         bla = get_users(conn)
         print(bla)
